332ce98ce1b79300a94da04b2be313f3ae1c7181
Kangawooo <kangawoooyt@gmail.com>
2026-07-05 03:16:26 +0100

    Added back and forth

--- src/js/ArtworkViewer.js
+++ src/js/ArtworkViewer.js
@@ -1,12 +1,35 @@
-window.onload = function () {
-    const artwork = document.getElementById("artwork-viewer");
-    const sensitivity = 30;
+const sensitivity = 30;
+var mouse_x = 0;
+var mouse_y = 0;
 
+const artwork_map = new Map([
+    [0, ["cammie1", "Cammie and Crycatt", "cammiescorner"]],
+    [1, ["carrion1", "Gossip Gills", "_c4rri0n"]],
+    [2, ["hoku1", "The Copper Cat", "Overcooked_rock"]],
+    [3, ["hoku2", "Hoku the Traveling Musician", "HokuIsAfish"]],
+    [4, ["lan1", "Become Memory", "Lan"]],
+    [5, ["mirko1", "Final moments", "mirk0oo"]],
+    [6, ["rainbells1", "The kingdom of Solaria", "rainbells"]],
+]);
+
+
+window.onload = function() {
+    const art_params = new URLSearchParams(window.location.search)
+    var artwork_key = parseInt(art_params.get('artwork'))
+
+    const artwork_viewer = document.getElementById("artwork-viewer");
+    const artwork = document.getElementById("artwork");
+    const title = document.getElementById("title");
+    const credit = document.getElementById("credit");
+    
+    update_artwork(artwork_key, artwork_viewer)
+
+    
     document.addEventListener('mousemove', function(event) {
-        var mouse_x = 0.5 - (event.clientX / window.innerWidth);
-        var mouse_y = 0.5 - (event.clientY / window.innerHeight);
+        mouse_x = 0.5 - (event.clientX / window.innerWidth);
+        mouse_y = 0.5 - (event.clientY / window.innerHeight);
 
-        artwork.style.transform = `translate(-50%, -50%) perspective(50cm) rotateX(${mouse_y * sensitivity}deg) rotateY(${-mouse_x * sensitivity}deg) `
+        artwork_viewer.style.transform = `translate(-50%, -50%) perspective(50cm) rotateX(${mouse_y * sensitivity}deg) rotateY(${-mouse_x * sensitivity}deg) `
     });
 
     const socials = document.querySelectorAll('.social');
@@ -21,10 +44,31 @@ window.onload = function () {
 
     const left_arrow = document.getElementById('left-arrow');
     left_arrow.addEventListener("click",function(){
-        
+        if (artwork_key <= 0) {
+            artwork_key = artwork_map.size
+        }
+        window.location.replace("?artwork=" + String(artwork_key - 1))
+    });
+
+    const right_arrow = document.getElementById('right-arrow');
+    right_arrow.addEventListener("click",function(){
+        if (artwork_key >= artwork_map.size-1) {
+            artwork_key = 0
+        }
+        window.location.replace("?artwork=" + String(artwork_key + 1))
     });
 }
 
 
 
+function update_artwork(artwork_key, artwork_viewer) {
+    if (artwork_key == null) {
+        window.location.replace("?artwork=0")
+    } else {
+        artwork.src = '/assets/images/artwork/' + artwork_map.get(artwork_key)[0] + '.png'
+        artwork_viewer.style.transform = `translate(-50%, -50%) perspective(50cm) rotateX(${mouse_y * sensitivity}deg) rotateY(${-mouse_x * sensitivity}deg) `
+        title.innerHTML  = artwork_map.get(artwork_key)[1]
+        credit.innerHTML  = "Artwork by " + artwork_map.get(artwork_key)[2]
+    }
+}
 
